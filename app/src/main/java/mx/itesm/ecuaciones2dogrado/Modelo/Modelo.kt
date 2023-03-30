631fe7795600edc3d6402f74c3ba561ec1e7694b
LBanda <A01610329@itesm.mx>
2023-03-30 12:07:14 -0600

    realizacion del modelo inicio del viewmodel

--- app/src/main/java/mx/itesm/ecuaciones2dogrado/Modelo/Modelo.kt
+++ app/src/main/java/mx/itesm/ecuaciones2dogrado/Modelo/Modelo.kt
@@ -1,4 +1,21 @@
 package mx.itesm.ecuaciones2dogrado.Modelo
 
+import kotlin.math.sqrt
+
 class Modelo {
+
+    var x1: String? = null
+    var x2: String? = null
+
+    fun solveEquation(a: Double, b: Double, c: Double) {
+        val discriminant = b * b - 4 * a * c
+
+        if (discriminant < 0) {
+            x1 = "Complejo"
+            x2 = "Complejo"
+        } else {
+            x1 = ((-b + sqrt(discriminant)) / (2 * a)).toString()
+            x2 = ((-b - sqrt(discriminant)) / (2 * a)).toString()
+        }
+    }
 }
--- app/src/main/java/mx/itesm/ecuaciones2dogrado/Viewmodel/VM.kt
+++ app/src/main/java/mx/itesm/ecuaciones2dogrado/Viewmodel/VM.kt
@@ -1,4 +1,14 @@
 package mx.itesm.ecuaciones2dogrado.Viewmodel
 
-class VM {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import mx.itesm.ecuaciones2dogrado.Modelo.Modelo
+
+class VM : ViewModel(){
+
+    private val modelo = Modelo()
+
+    val a = MutableLiveData<Double>(0.0)
+    val b = MutableLiveData<Double>(0.0)
+    val c = MutableLiveData<Double>(0.0)
 }
